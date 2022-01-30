c090720420387e2245ef1a7844e2ebdbbfd614d9
vBusch95 <janole.wiebusch@t-online.de>
2022-01-30 17:53:56 +0100

    updated to redis cloud

--- src/utils/pubsub.js
+++ src/utils/pubsub.js
@@ -1,16 +1,25 @@
 const { RedisPubSub } = require("graphql-redis-subscriptions");
 const Redis = require("ioredis");
+require("dotenv").config();
 
 const REDIS_DOMAIN_NAME = "127.0.0.1";
 const PORT_NUMBER = 6379;
 
+const REDIS_PORT = 16638;
+// const options = {
+//   host: REDIS_DOMAIN_NAME,
+//   port: PORT_NUMBER,
+//   retryStrategy: (times) => {
+//     // reconnect after
+//     return Math.min(times * 100, 3000);
+//   },
+// };
+
 const options = {
-  host: REDIS_DOMAIN_NAME,
-  port: PORT_NUMBER,
-  retryStrategy: (times) => {
-    // reconnect after
-    return Math.min(times * 100, 3000);
-  },
+  host: process.env.REDIS_HOST,
+  port: 16638,
+  password: process.env.REDIS_PW,
+  username: "default",
 };
 
 const pubsub = new RedisPubSub({
