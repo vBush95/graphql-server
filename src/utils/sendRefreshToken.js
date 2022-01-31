9ad0e9a7b587bda9f15ac518893c6a049529d994
vBusch95 <janole.wiebusch@t-online.de>
2022-01-31 12:52:32 +0100

    added samsite none to cookie

--- src/utils/sendRefreshToken.js
+++ src/utils/sendRefreshToken.js
@@ -1,5 +1,7 @@
 const sendRefreshToken = (res, refreshToken) => {
   res.cookie(process.env.SECRET_KEY_COOKIE, refreshToken, {
+    sameSite: "none",
+    secure: true,
     httpOnly: true,
     path: "/refresh_token",
   });
