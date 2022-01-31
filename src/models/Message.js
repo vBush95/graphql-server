1d6ab26afc2df5e1a9c51b4333a8584acc5e7693
vBusch95 <janole.wiebusch@t-online.de>
2022-01-31 09:53:27 +0100

    updated db expiring delay

--- src/models/Message.js
+++ src/models/Message.js
@@ -4,9 +4,9 @@ const messageSchema = new Schema({
   username: String,
   content: String,
   // delets message after x seconds
-  createdAt: { type: Date, expires: 120, default: Date.now },
+  createdAt: { type: Date, expires: 86400, default: Date.now },
 });
 
-messageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 120 });
+messageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 });
 
 module.exports = model("Message", messageSchema);
